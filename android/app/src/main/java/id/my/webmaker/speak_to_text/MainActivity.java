d890c8cf1589f89a883a5ce8ba87dd3792f05cf3
Arinadi Nur Rohmad <arinadi.nur@gmail.com>
2021-02-19 06:41:52 +0700

    Initial commit

---
+++ android/app/src/main/java/id/my/webmaker/speak_to_text/MainActivity.java
@@ -0,0 +1,6 @@
+package id.my.webmaker.speak_to_text;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,205 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_tts_improved/flutter_tts_improved.dart';
+import 'package:highlight_text/highlight_text.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Echo (STT > TTS)',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SpeechScreen(),
+    );
+  }
+}
+
+class SpeechScreen extends StatefulWidget {
+  @override
+  _SpeechScreenState createState() => _SpeechScreenState();
+}
+
+class _SpeechScreenState extends State<SpeechScreen> {
+  final Map<String, HighlightedWord> _highlights = {
+    'flutter': HighlightedWord(
+      onTap: () => print('flutter'),
+      textStyle: const TextStyle(
+        color: Colors.blue,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    'voice': HighlightedWord(
+      onTap: () => print('voice'),
+      textStyle: const TextStyle(
+        color: Colors.green,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    'subscribe': HighlightedWord(
+      onTap: () => print('subscribe'),
+      textStyle: const TextStyle(
+        color: Colors.red,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    'like': HighlightedWord(
+      onTap: () => print('like'),
+      textStyle: const TextStyle(
+        color: Colors.blueAccent,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    'comment': HighlightedWord(
+      onTap: () => print('comment'),
+      textStyle: const TextStyle(
+        color: Colors.green,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    'ikan': HighlightedWord(
+      onTap: () => print('ikan'),
+      textStyle: const TextStyle(
+        color: Colors.green,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    'makan': HighlightedWord(
+      onTap: () => print('makan'),
+      textStyle: const TextStyle(
+        color: Colors.red,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  };
+
+  stt.SpeechToText _speech;
+  FlutterTtsImproved tts = FlutterTtsImproved();
+  String _platformVersion = 'Unknown';
+  bool _isListening = false;
+  bool _preparationProcess = false;
+  String _text = 'Press the button and start speaking';
+  double _confidence = 1.0;
+
+  @override
+  void initState() {
+    print('INIT STATE');
+    super.initState();
+    _speech = stt.SpeechToText();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("BUILD STATE");
+    print("_isListening $_isListening");
+    print("_preparationProcess $_preparationProcess");
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Confidence: ${(_confidence * 100.0).toStringAsFixed(1)}%'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        animate: _isListening,
+        glowColor: Theme.of(context).primaryColor,
+        endRadius: 75.0,
+        duration: const Duration(milliseconds: 2000),
+        repeatPauseDuration: const Duration(milliseconds: 100),
+        repeat: true,
+        child: FloatingActionButton(
+          onPressed: _listen,
+          child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+          backgroundColor: _isListening ? Colors.blue : Colors.red,
+        ),
+      ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 150.0),
+          child: TextHighlight(
+            text: _text,
+            words: _highlights,
+            textStyle: const TextStyle(
+              fontSize: 32.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (val) {
+          print('onStatus: $val');
+
+          if (val == 'listening') {
+            setState(() {
+              _isListening = true;
+              _preparationProcess = true;
+            });
+          } else {
+            setState(() {
+              _isListening = false;
+            });
+          }
+        },
+        onError: (val) {
+          print('onError: $val');
+          setState(() {
+            _isListening = false;
+            _preparationProcess = false;
+          });
+        },
+      );
+
+      if (available) {
+        _speech.listen(
+          onResult: (val) => setState(() {
+            _text = val.recognizedWords;
+            if (val.hasConfidenceRating && val.confidence > 0) {
+              _confidence = val.confidence;
+            }
+            if (!_isListening && _preparationProcess) {
+              _speech.stop();
+              _speak();
+              setState(() {
+                _preparationProcess = false;
+              });
+            }
+          }),
+          localeId: "id-ID",
+        );
+      }
+    }
+  }
+
+  void _speak() async {
+    if (!mounted) return;
+
+    // print('VOICES: ${await tts.getVoices}');
+    // print('LANGUAGES: ${await tts.getLanguages}');
+    //
+    // tts.setProgressHandler((String words, int start, int end, String word) {
+    //   setState(() {
+    //     _platformVersion = word;
+    //   });
+    //   print('PROGRESS: $word => $start - $end');
+    // });
+    tts.setLanguage('id-ID');
+    await tts.speak(_text);
+    print(_text);
+  }
+}
